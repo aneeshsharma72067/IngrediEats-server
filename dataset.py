bf6bd772bcdfa496e93d07e15962d9d05ee9daee
Aneesh Sharma <as416106@gmail.com>
2023-12-05 08:07:31 +0530

    sending ingredients

--- dataset.py
+++ dataset.py
@@ -6,6 +6,7 @@ df = pd.read_csv('recipe_dataset.csv')
 recipe_names = list(df['Title'])
 recipe_image_names = list(df['Image_Name'])
 recipe_id = list(df['Index'])
+ingredients = list(df['Ingredients'])
 
 def getRecipeNames():
-    return [{'id':recipe_id[i], 'recipe_name':recipe_names[i],'image_name':recipe_image_names[i]} for i in range(0,1000)]
+    return [{'id':recipe_id[i], 'recipe_name':recipe_names[i],'image_name':recipe_image_names[i], 'ingredients':eval(ingredients[i])} for i in range(0,50)]
